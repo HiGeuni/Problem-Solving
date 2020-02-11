00308c065b3479e5a1f80a155dd9b1be5838740b
geunnii <59858440+geunnii@users.noreply.github.com>
2020-02-11 23:01:19 +0900

    BOJ

---
+++ divide and conquer/1780.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int arr[2187][2187];
+int res[3];
+
+void func(int n, int y, int x){
+	int color = arr[y][x];
+	for(int i=x; i<x+n; ++i){
+		for(int j=y; j<y+n; ++j){
+			if(color != arr[j][i]){
+				func(n/3, y, x);
+				func(n/3, y+n/3, x);
+				func(n/3, y+(2*n)/3, x);
+				func(n/3, y, x+n/3);
+				func(n/3, y+n/3, x+n/3);
+				func(n/3, y+(2*n)/3, x+n/3);
+				func(n/3, y, x+(2*n)/3);
+				func(n/3, y+n/3, x+(2*n)/3);
+				func(n/3, y+(2*n)/3, x+(2*n)/3);
+				return;
+			}
+		}
+	}
+	res[color+1]++;
+}
+
+int main(){
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+	for(int i=0; i<n; ++i){
+		for(int j=0; j<n; ++j){
+			cin>>arr[j][i];
+		}
+	}
+	func(n, 0, 0);
+	for(int i=0; i<3; ++i){
+		cout<<res[i]<<endl;
+	}
+}
