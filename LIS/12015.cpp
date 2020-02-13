87689c7a017e8ec3514b16e222aacb1dedf9d506
geunnii <59858440+geunnii@users.noreply.github.com>
2020-02-13 19:18:55 +0900

    ㅠㅒㅖ

---
+++ LIS/12015.cpp
@@ -0,0 +1,29 @@
+//12015 가장 긴 증가하는 수열2
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> LIS(vector<int> &v){
+	vector<int>ans(1,0);
+	for(int i =0 ;i<v.size(); ++i){
+		if(v[i] > ans.back()){
+			ans.push_back(v[i]);
+		}
+		else{
+			vector<int>::iterator it = lower_bound(ans.begin(), ans.end(), v[i]);
+			*it = v[i];
+		}
+	}
+	return ans;
+}
+
+int main(){
+	int n;
+	cin>>n;
+	vector<int>v(n);
+	for(int i=0; i<n; ++i)
+		cin>>v[i];
+	vector<int>ans = LIS(v);
+	cout<<ans.size()-1;
+	return 0;
+}
