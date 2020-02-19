dd481ccea189cd08dafa3f81af06ad2a5e8975a4
geunnii <59858440+geunnii@users.noreply.github.com>
2020-02-19 22:58:40 +0900

    Create 2775.py

---
+++ implement/2775.py
@@ -0,0 +1,12 @@
+#2775 부녀회장이 될테야
+#20200219
+
+tc = int(input())
+for _ in range(tc):
+    k = int(input())
+    n = int(input())
+    ar = [ i for i in range(1, n+1)]
+    for _ in range(k):
+        for j in range(1,n):
+            ar[j] += ar[j-1]
+    print(ar[-1])
