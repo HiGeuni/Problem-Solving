90c0566ecab55c574b54de42bbdd981ab2f13f1e
geunnii <59858440+geunnii@users.noreply.github.com>
2020-02-21 18:30:56 +0900

    Create 2953.cpp

---
+++ implement/2953.cpp
@@ -0,0 +1,22 @@
+//2953 나는 요리사다
+//20200221
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int score[4], ans = 0, tmp, num;
+	for(int i=0; i<5; ++i){
+		tmp =0;
+		for(int j=0; j<4; ++j){
+			cin>>score[j];
+			tmp+=score[j];
+		}
+		if(ans <tmp){
+			num = i+1;
+			ans = tmp;
+		}
+	}
+	cout<<num<<" "<<ans<<endl;
+	return 0;
+}
