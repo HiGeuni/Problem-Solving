561c32ebbe03e63984edb5f53f33a194a49072af
geunnii <59858440+geunnii@users.noreply.github.com>
2020-02-26 14:47:10 +0900

    BOJ

---
+++ implement/5337.cpp
@@ -0,0 +1,11 @@
+//5337 welcome
+//20200225
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<".  .   ."<<endl;
+	cout<<"|  | _ | _. _ ._ _  _"<<endl;
+	cout<<"|/\\\|(/.|(_.(_)[ | )(/."<<endl;
+	return 0;
+}
