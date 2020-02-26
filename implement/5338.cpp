04437996137c008d5fff8f7d774024eefcd0a2f2
geunnii <59858440+geunnii@users.noreply.github.com>
2020-02-26 14:44:16 +0900

    BOJ

---
+++ implement/5338.cpp
@@ -0,0 +1,14 @@
+//5338 마이크로소프트 로고
+//20200225
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"       _.-;;-._"<<endl;
+	cout<<"'-..-'|   ||   |"<<endl;
+	cout<<"'-..-'|_.-;;-._|"<<endl;
+	cout<<"'-..-'|   ||   |"<<endl;
+	cout<<"'-..-'|_.-''-._|"<<endl;
+	return 0;
+}
