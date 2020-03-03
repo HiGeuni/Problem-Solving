7ff0e0f9878fb8afc812c5bbb4bd899464aee9e5
geunnii <59858440+geunnii@users.noreply.github.com>
2020-03-03 11:56:34 +0900

    BOJ
    Runtime Error:
    처음에 else문의 print 들여쓰기를 안했다

---
+++ implement/1059.py
@@ -0,0 +1,20 @@
+#1059 수 2
+#20200303
+
+l = int(input())
+ls = sorted(list(map(int, input().split())))
+n = int(input())
+
+s =0
+
+if n in ls:
+    print(0)
+
+else:
+    for i in ls:
+        if n > i:
+            s = i+1
+        else:
+            e = i-1
+            break
+    print(e-s + (e-n)*(n-s))
