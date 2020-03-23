a8bff11c0ffe57397fbb7bd519837a70b9640df9
geunnii <59858440+geunnii@users.noreply.github.com>
2020-03-23 19:18:01 +0900

    BOJ

---
+++ implement/10869.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i,j;
+    scanf("%d%d", &i,&j);
+    printf("%d\n", i+j);
+    printf("%d\n", i-j);
+    printf("%d\n", i*j);
+    printf("%d\n", i/j);
+    printf("%d\n", i%j);
+    return 0;
+}
