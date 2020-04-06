75d7efe49b2f087cc28e50c33f8b1482427a2204
geunnii <59858440+geunnii@users.noreply.github.com>
2020-04-06 18:53:15 +0900

    BOJ

---
+++ implement/1965.cpp
@@ -0,0 +1,33 @@
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int>seq;
+int d[1234];
+int T;
+
+int LIS(int start) {
+	if (start >= T)
+		return 0;
+	if (d[start] != 0) return d[start];
+	d[start] = 1;
+	for (int i = start + 1; i < T; i++) {
+		if (seq[start] < seq[i]) {
+			d[start] = max(d[start], LIS(i) + 1);
+		}
+	}
+	return d[start];
+}
+
+int main() {
+	int tmp;
+	scanf("%d", &T);
+	for (int i = 0; i < T; i++) {
+		scanf("%d", &tmp);
+		seq.push_back(tmp);
+	}
+	int ans = 0;
+	for (int i = 0; i < T; ++i)
+		ans = max(ans, LIS(i));
+	printf("%d", ans);
+}
