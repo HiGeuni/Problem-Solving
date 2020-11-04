3019470ba42be7bfe1dd366a90999a5cb7436570
geunnii <59858440+geunnii@users.noreply.github.com>
2020-11-04 23:00:51 +0900

    BOJ

---
+++ BaekJoon/15652.py
@@ -0,0 +1,24 @@
+def recursive(ans):
+    
+    if len(ans) == m:
+        for i in range(m):
+            print(ans[i], end = ' ')
+        print()
+        return
+    back = ans[-1]
+    newlst = ans[:]
+ 
+    for i in range(1, n+1):
+        if i >= back:
+            newlst.append(i)
+            recursive(newlst)
+            newlst.pop()
+
+
+n,m = map(int, input().split())
+ls = []
+for i in range(n):
+    ls.append(i+1)
+    recursive(ls)
+    ls.pop()
+    
