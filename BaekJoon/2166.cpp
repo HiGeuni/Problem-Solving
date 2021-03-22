902758a44d9060be5a80a0f8d8149ead72beff13
geunnii <khyogeun_@naver.com>
2021-03-22 22:48:34 +0900

    BOJ

---
+++ BaekJoon/2166.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  First_C++
+//
+//  Created by 김효근 on 2021/03/06.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int n;
+    cin>>n;
+    cout<<n;
+    return 0;
+}
