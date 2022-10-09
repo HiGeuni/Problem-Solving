8d1c4c6f5c31ba1e88b3ecc2b397190290f4ba7c
Kim HyoGeun <59858440+HiGeuni@users.noreply.github.com>
2022-10-09 16:02:36 +0900

    eddited

--- BaekJoon/1305.cpp
+++ BaekJoon/1305.cpp
@@ -1,27 +1,29 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
 
-vector<int> makeTable(string pattern){
+vector<int> makeTable(string pattern) {
     int lenPat = pattern.size();
     vector<int>v(lenPat);
     // i == pattern, j == continuous
-    int i,j=0;
-    for(i=1; i<lenPat; ++i){
-        while(j > 0 && pattern[i] != pattern[j]){
-            j = v[j-1];
+    int j = 0;
+    for (int i = 1; i < lenPat; ++i) {
+        while (j > 0 && pattern[i] != pattern[j]) {
+            j = v[j - 1];
         }
-        if(pattern[i] == pattern[j])
+        if (pattern[i] == pattern[j])
             v[i] = ++j;
     }
     return v;
 }
 
-int main(){
+int main() {
     int n;
-    cin>>n;
+    cin >> n;
     string a;
-    cin>>a;
+    cin >> a;
     vector<int>ans = makeTable(a);
-    cout<<n - ans[n-1];
+    cout << n - ans[n - 1];
     return 0;
 }
