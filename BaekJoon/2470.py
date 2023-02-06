7107dc0956477330bf6fe9f601df223d88e7f57a
Kim HyoGeun <59858440+HiGeuni@users.noreply.github.com>
2023-02-06 23:42:39 +0900

    Create 2470.py

---
+++ BaekJoon/2470.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+ls = sorted(list(map(int,input().split())))
+
+l = 0
+r = len(ls)-1
+ans = [2e9 + 1, 0, 0]
+
+while l < r:
+    tmp = ls[l] + ls[r]
+    if abs(tmp) < abs(ans[0]):
+        ans = [tmp, ls[l], ls[r]]
+    if tmp < 0:
+        l += 1
+    else:
+        r -= 1
+
+print(ans[1], ans[2])
